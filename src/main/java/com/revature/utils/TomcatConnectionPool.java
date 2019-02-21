25e03d5ba7b1bbff33b48e8396ef5fef4f25d900
Brock Sokevitz <bsokevitz@gmail.com>
2019-02-21 08:42:38 -0500

    added some connection limiters

--- src/main/java/com/revature/utils/TomcatConnectionPool.java
+++ src/main/java/com/revature/utils/TomcatConnectionPool.java
@@ -11,42 +11,42 @@ import javax.sql.DataSource;
 import org.apache.log4j.Logger;
 
 public class TomcatConnectionPool {
-	
-	   private static TomcatConnectionPool pool = null;
-	    private static DataSource dataSource = null;
 
-	    private TomcatConnectionPool() {
-	        try {
-	            InitialContext initialContext = new InitialContext();
-	            Context environmentContext = (Context) initialContext.lookup("java:comp/env");
-				dataSource = (DataSource) environmentContext.lookup("jdbc/Database");
-	        } catch (NamingException e) {
-	            System.out.println(e);
-	        }
-	    }
+	private static TomcatConnectionPool pool = null;
+	private DataSource dataSource = null;
 
-	    public static synchronized TomcatConnectionPool getInstance() {
-	        if (pool == null) {
-	            pool = new TomcatConnectionPool();
-	        }
-	        return pool;
-	    }
+	private TomcatConnectionPool() {
+		try {
+			InitialContext initialContext = new InitialContext();
+			Context environmentContext = (Context) initialContext.lookup("java:comp/env");
+			dataSource = (DataSource) environmentContext.lookup("jdbc/Database");
+		} catch (NamingException e) {
+			System.out.println(e);
+		}
+	}
 
-	    public Connection getConnection() {
-	        try {
-	            return dataSource.getConnection();
-	        } catch (SQLException e) {
-	            System.out.println(e);
-	            return null;
-	        }
-	    }
+	public static synchronized TomcatConnectionPool getInstance() {
+		if (pool == null) {
+			pool = new TomcatConnectionPool();
+		}
+		return pool;
+	}
 
-	    public void freeConnection(Connection c) {
-	        try {
-	            c.close();
-	        } catch (SQLException e) {
-	            System.out.println(e);
-	        }
-	    }
-	
+	public Connection getConnection() {
+		try {
+			return dataSource.getConnection();
+		} catch (SQLException e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+
+	public void freeConnection(Connection c) {
+		try {
+			c.close();
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+	}
+
 }
